1c111a771b90181b714dc337fdcfd5e7fc640aa5
jasonChen1982 <jasonchen1882@gmail.com>
2018-05-24 18:01:06 +0800

    feat(extract): add a param to control just extract main file or not

--- src/actions/extract/index.js
+++ src/actions/extract/index.js
@@ -6,28 +6,40 @@ const cwd = process.cwd();
 const projectRoot = path.basename(cwd);
 const fromPrefix = path.resolve(cwd, 'node_modules');
 
-function copyModules(output, mdl) {
+function copyModules(output, mdn, slim) {
   const toPrefix = path.resolve(cwd, output);
-  const from = path.join(fromPrefix, mdl);
-  const to = path.join(toPrefix, mdl);
+  const moduleRoot = path.join(fromPrefix, mdn);
+
+  let from = path.join(fromPrefix, mdn);
+  let to = path.join(toPrefix, mdn);
+
+  if (slim) {
+    const package = require(path.join(moduleRoot, 'package.json'));
+    const build = path.dirname(package.main);
+    from = path.join(from, build);
+    to = path.join(to, build);
+  }
+
   const taskTips = chalk.yellow(`from ${path.join(projectRoot, 'node_modules')} to ${path.join(projectRoot, output)}`);
+
   return fse.copy(from, to)
     .then(() => {
-      console.log(chalk.green(`copy ${mdl} success, ${taskTips}`));
+      console.log(chalk.green(`copy ${mdn} success, ${taskTips}`));
     })
     .catch(err => {
-      console.log(chalk.red(`copy ${mdl} error, ${taskTips}`));
+      console.log(chalk.red(`copy ${mdn} error, ${taskTips}`));
       console.error(err);
     });
 }
 
-module.exports = function() {
+module.exports = function(argv) {
+  const slim = argv.slim;
   const pkg = util.getPkg();
   const npmsrcifyConfig = pkg.config.npmsrcify;
   const rawModules = npmsrcifyConfig.modules;
   const output = npmsrcifyConfig.output || './library';
-  const tasks = rawModules.map(mdl => {
-    return copyModules(output, mdl);
+  const tasks = rawModules.map(mdn => {
+    return copyModules(output, mdn, slim);
   });
   return Promise.all(tasks)
     .then(() => {
--- src/cmds/extract.js
+++ src/cmds/extract.js
@@ -4,7 +4,14 @@ const extract = require('../actions/extract/index');
 
 exports.command = 'extract';
 exports.desc = '提取文件到指定目录';
-exports.builder = {};
+exports.builder = yargs => {
+  return yargs
+    .option('slim', {
+      alias: 's',
+      describe: '是否只提取生产文件',
+      default: true,
+    });
+};
 exports.handler = function(argv) {
-  extract();
+  extract(argv);
 };
